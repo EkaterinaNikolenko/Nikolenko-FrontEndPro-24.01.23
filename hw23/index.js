6bd7d6a5dbcb04fd6ffd4665f1a9348994d15efe
Ekaterina Nikolenko <nikolenkoekaterina@gmail.com>
2023-03-02 14:40:41 +0200

    hw23

---
+++ hw23/index.js
@@ -0,0 +1,8 @@
+let div = document.body.childNodes[1];
+let ul = document.querySelector('ul');
+let li = document.body.children[1].children[1];
+let li2 = document.body.querySelector('ul>li:nth-child(2)');
+console.log(div);
+console.log(ul);
+console.log(li);
+console.log(li2);
