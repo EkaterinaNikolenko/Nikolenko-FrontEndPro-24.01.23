a9fb26efe031da26440bf7c88147f937820d47e1
Ekaterina Nikolenko <nikolenkoekaterina@gmail.com>
2023-03-02 15:46:52 +0200

    hw25

--- hw25/index.js
+++ hw25/index.js
@@ -1,12 +1,11 @@
 let table = document.createElement('table');
-
 let k = 0
 for(let i = 0; i < 10; i++){
     let tr = document.createElement('tr');
     table.append(tr);
     for(let j = k; j < k + 10; j++){
         let td = document.createElement('td');
-        td.innerHTML = `${j + 1}`;
+        td.innerHTML = j + 1;
         tr.append(td);
     }
     k += 10;
