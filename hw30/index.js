8d7ac375e56e3f6f930cbb96f3efc79841788051
Ekaterina Nikolenko <nikolenkoekaterina@gmail.com>
2023-03-12 16:15:16 +0200

    hw30

---
+++ hw30/index.js
@@ -0,0 +1,24 @@
+const array = [1, 2, 3, [4.1, 4.2], 5, [6.1, 6.2, 6.3], 7];
+
+function generateList(array) {
+    let ul = document.createElement("ul");
+    for(let item of array){
+        let li = document.createElement("li");
+        if (Array.isArray(item)){
+            let ul2 = document.createElement("ul");
+            for(let item2 of item){
+                let li2 = document.createElement("li");
+                li2.innerText = item2;
+                ul2.appendChild(li2);
+            }
+            li.appendChild(ul2);
+        }
+        else{
+            li.innerText = item;
+        }
+        ul.appendChild(li);
+    }
+    document.body.appendChild(ul);
+}
+
+generateList(array);
